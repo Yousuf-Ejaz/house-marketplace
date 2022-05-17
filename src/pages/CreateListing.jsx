42fd0658eccc66b6999316ef1420ae5d7a419b79
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2022-05-17 14:00:03 +0530

    Create Listings Form Completed with Image Upload Func

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -15,7 +15,7 @@ import Spinner from "../components/Spinner";
 
 function CreateListing() {
 	// eslint-disable-next-line
-	const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+	const [geolocationEnabled, setGeolocationEnabled] = useState(false);
 	const [loading, setLoading] = useState(false);
 	const [formData, setFormData] = useState({
 		type: "rent",
@@ -161,7 +161,7 @@ function CreateListing() {
 			});
 		};
 
-		const imgUrls = await Promise.all(
+		const imageUrls = await Promise.all(
 			[...images].map((image) => storeImage(image))
 		).catch(() => {
 			setLoading(false);
@@ -171,7 +171,7 @@ function CreateListing() {
 
 		const formDataCopy = {
 			...formData,
-			imgUrls,
+			imageUrls,
 			geolocation,
 			timestamp: serverTimestamp(),
 		};
