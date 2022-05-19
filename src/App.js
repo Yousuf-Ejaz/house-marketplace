b00f6ed742a27bb7b2df4b6d0eb57db11e649950
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2022-05-19 12:19:33 +0530

    Added Landlord Contact Page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
 	return (
@@ -32,6 +34,11 @@ function App() {
 						element={<ForgotPassword />}
 					/>
 					<Route path="/create-listing" element={<CreateListing />} />
+					<Route
+						path="/category/:categoryName/:listingId"
+						element={<Listing />}
+					/>
+					<Route path="/contact/:landlordId" element={<Contact />} />
 				</Routes>
 				<Navbar />
 			</Router>
