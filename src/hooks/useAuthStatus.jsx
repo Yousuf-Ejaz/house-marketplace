a3c84474768ed043cc04565a3894e5b42beafa8b
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2022-05-20 03:25:37 +0530

    Added Profile listing Delete and Edit

--- src/hooks/useAuthStatus.jsx
+++ src/hooks/useAuthStatus.jsx
@@ -1,17 +1,22 @@
+import { useEffect, useState, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useEffect, useRef, useState } from "react";
 
 export const useAuthStatus = () => {
-	const auth = getAuth();
 	const [loggedIn, setLoggedIn] = useState(false);
 	const [checkingStatus, setCheckingStatus] = useState(true);
 	const isMounted = useRef(true);
 
 	useEffect(() => {
-		onAuthStateChanged(auth, (user) => {
-			if (user) setLoggedIn(true);
-			setCheckingStatus(false);
-		});
+		if (isMounted) {
+			const auth = getAuth();
+			onAuthStateChanged(auth, (user) => {
+				if (user) {
+					setLoggedIn(true);
+				}
+				setCheckingStatus(false);
+			});
+		}
+
 		return () => {
 			isMounted.current = false;
 		};
@@ -19,3 +24,5 @@ export const useAuthStatus = () => {
 
 	return { loggedIn, checkingStatus };
 };
+
+
